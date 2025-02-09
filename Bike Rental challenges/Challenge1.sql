bd482999fe148f3f18e6b6160ea228e95fac9362
Sudeshna B <sudeshnab@Sudeshnas-MacBook-Air.local>
2025-02-09 22:46:27 +0530

    Updating as on 09-02-2025

--- Bike Rental challenges/Challenge1.sql
+++ Bike Rental challenges/Challenge1.sql
@@ -21,3 +21,48 @@ select * from rental;
 select * from membership_type;
 select * from membership;
 
+--1st problem solved
+select
+    category
+    ,count(category) as numberofbikes 
+from
+    bike
+group by
+    category
+having
+    count(category)>2
+
+--2nd problem solved
+-- Left join gives the remaining customer names who do not have any membership
+select
+    c.name,
+    count(m.customer_id) as membership_count
+from
+    customer as c
+left join
+    membership as m
+on
+    m.customer_id = c.id
+group by
+    c.name
+    ,m.customer_id
+order by    --alias also works for order by
+    membership_count desc 
+
+/* Emily is working on a special offer for the winter months. Can you help her
+prepare a list of new rental prices?
+For each bike, display its ID, category, old price per hour (call this column
+old_price_per_hour ), discounted price per hour (call it new_price_per_hour ), old
+price per day (call it old_price_per_day ), and discounted price per day (call it
+new_price_per_day ).Electric bikes should have a 10% discount for hourly rentals 
+and a 20% discount for daily rentals. Mountain bikes should have a 20% discount for
+hourly rentals and a 50% discount for daily rentals. All other bikes should
+have a 50% discount for all types of rentals.
+Round the new prices to 2 decimal digits.*/
+
+/*4. Emily is looking for counts of the rented bikes and of the available bikes in
+each category.
+Display the number of available bikes (call this column 
+available_bikes_count ) 
+and the number of rented bikes (call this column rented_bikes_count )
+by bike category. */ 
